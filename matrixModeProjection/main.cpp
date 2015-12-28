208c7a20479f6c354e418054a688af554efed2d6
Lacty <akira206@gmail.com>
2015-12-28 16:15:24 +0900

    add matrixModeProjection

---
+++ matrixModeProjection/main.cpp
@@ -0,0 +1,58 @@
+
+#include <iostream>
+#include <cmath>
+#include <GLFW/glfw3.h>
+
+
+void resize(GLFWwindow* window, const int width, const int height) {
+  glViewport(0, 0, width, height);
+}
+
+int main() {
+  GLFWwindow* window;
+
+  if (!glfwInit()) {
+    return -1;
+  }
+
+  window = glfwCreateWindow(640, 480, "changeWindowSize", nullptr, nullptr);
+  if (!window) {
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+  glfwSetWindowSizeCallback(window, resize);
+
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+  
+  float angle = 1.0f;
+
+  while(!glfwWindowShouldClose(window)) {
+    glClearColor(0.4f, 0.4f, 0.4f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    GLfloat vtx[] = {
+      0.0f, 0.433f,
+      -0.5f, -0.433f,
+      0.5f, -0.433f
+    };
+
+    glRotatef(angle, 0.4f, 1.0f, 0.0f);
+
+    glVertexPointer(2, GL_FLOAT, 0, vtx);
+    
+    glEnableClientState(GL_VERTEX_ARRAY);
+    
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glDisableClientState(GL_VERTEX_ARRAY);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
+}
