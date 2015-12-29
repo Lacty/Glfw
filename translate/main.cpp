3507ce41ae971304d9f5f5b964af57c7e607096e
Lacty <akira206@gmail.com>
2015-12-29 22:05:55 +0900

    add translate

---
+++ translate/main.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+
+int main() {
+  if (!glfwInit()) return -1;
+
+  GLFWwindow* window;
+	window = glfwCreateWindow(640, 480, "translate", nullptr, nullptr);
+	if (!window) {
+	  glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	while(!glfwWindowShouldClose(window)) {
+	  glClearColor(0.4f, 0.4f, 0.4f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		GLfloat vtx[] = {
+		  0.0f, 0.433f,
+			-0.5f, -0.433f,
+			0.5f, -0.433f
+		};
+
+		glVertexPointer(2, GL_FLOAT, 0, vtx);
+
+		glEnableClientState(GL_VERTEX_ARRAY);
+    
+		glLoadIdentity();
+		glTranslatef(0.3f, 0.0f, 0.0f);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glDisableClientState(GL_VERTEX_ARRAY);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
