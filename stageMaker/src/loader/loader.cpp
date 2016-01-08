361f0b0164c13d9433deb217578d2cc6e9d92eaf
Lacty <akira206@gmail.com>
2016-01-09 07:54:59 +0900

    impl loader

--- stageMaker/src/loader/loader.cpp
+++ stageMaker/src/loader/loader.cpp
@@ -1,5 +1,65 @@
 
 #include "loader.hpp"
+#include <sstream>
+#include <fstream>
 
 
-Loader::Loader() {}
+Loader::Loader(const std::string& path) :
+path(path) {}
+
+
+void Loader::load() {
+  std::stringstream ss;
+  std::ifstream      f;
+
+  // load
+  f.open(path.c_str(), std::ios::binary);
+  if (!f) {
+    std::cout << "file not opened" << std::endl;
+  }
+
+  // convert to stringstream
+  ss << f.rdbuf();
+  f.close();
+
+  picojson::value v;
+  ss >> v;
+
+  auto obj = v.get<picojson::object>();
+  
+  // extraction "vertex"
+  auto array = obj["vertex"].get<picojson::array>();
+
+  vtx.clear();
+  for (auto it : array) {
+    vtx.push_back(it.get<double>());
+  }
+}
+
+void Loader::save(const std::vector<GLfloat>& vtx) {
+  picojson::object obj;
+  picojson::array  array;
+  
+  for (auto it : vtx) {
+    array.emplace_back(picojson::value(it));
+  }
+  
+  // make object
+  obj.insert(std::make_pair("vector", picojson::value(array)));
+
+  // convert to value
+  picojson::value json(obj);
+
+  // save
+  std::ofstream ofs(path.c_str());
+  ofs << json.serialize().c_str();
+}
+
+void Loader::setPath(const std::string& path) {
+  this->path = path;
+}
+
+
+const std::vector<GLfloat>& Loader::getVtx() const {
+  return vtx;
+}
--- stageMaker/src/loader/loader.hpp
+++ stageMaker/src/loader/loader.hpp
@@ -1,9 +1,21 @@
 
 #pragma once
+#include <vector>
+#include <GLFW/glfw3.h>
+#include <Pico/picojson.h>
 
 
 class Loader {
 private:
+  std::string          path;
+  std::vector<GLfloat> vtx;
+
 public:
-  Loader();
+  Loader(const std::string& path);
+
+  void load();
+  void save(const std::vector<GLfloat>& vtx);
+  void setPath(const std::string& path);
+
+  const std::vector<GLfloat>& getVtx() const;
 };
