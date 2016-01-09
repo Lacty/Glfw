acac277e48250068422f4e2cde6f838107de1974
Lacty <akira206@gmail.com>
2016-01-09 18:19:45 +0900

    impl transMatrix

--- stageMaker/src/utility/utility.cpp
+++ stageMaker/src/utility/utility.cpp
@@ -24,3 +24,13 @@ Eigen::Matrix3f rotMatrix(float x, float y, float z) {
 
   return matX * matY * matZ;
 }
+
+Eigen::Matrix4f transMatrix(float x, float y, float z) {
+  Eigen::Matrix4f m;
+  m << 1, 0, 0, x,
+       0, 1, 0, y,
+       0, 0, 1, z,
+       0, 0, 0, 1;
+
+  return m;
+}
--- stageMaker/src/utility/utility.hpp
+++ stageMaker/src/utility/utility.hpp
@@ -6,4 +6,6 @@
 
 float toRadians(float deg);
 
-Eigen::Matrix4f rotMatrix(float x, float y, float z);
+Eigen::Matrix3f rotMatrix(float x, float y, float z);
+
+Eigen::Matrix4f transMatrix(float x, float y, float z);
