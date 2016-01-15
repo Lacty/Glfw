de999a77045418c58a1805b80f449e7a37c684a3
Lacty <akira206@gmail.com>
2016-01-15 23:47:55 +0900

    update

--- antTweekBar/main.cpp
+++ antTweekBar/main.cpp
@@ -4,8 +4,18 @@
 #include <GLFW/glfw3.h>
 
 
+inline void TwEventMouseButtonGLFW3(GLFWwindow* window, int button, int action, int mods)
+{ TwEventMouseButtonGLFW(button, action); }
+inline void TwEventMousePosGLFW3(GLFWwindow* window, double xpos, double ypos)
+{ TwMouseMotion(int(xpos), int(ypos)); }
+inline void TwEventMouseWheelGLFW3(GLFWwindow* window, double xoffset, double yoffset)
+{ TwMouseWheel(yoffset); }
+inline void TwEventKeyGLFW3(GLFWwindow* window, int key, int scancode, int action, int mods)
+{ TwEventKeyGLFW(key, action); }
+inline void TwEventCharGLFW3(GLFWwindow* window, int codepoint)
+{ TwEventCharGLFW(codepoint, GLFW_PRESS); }
 inline void TwEventWindowSizeGLFW3(GLFWwindow* window, int width, int height)
-{TwWindowSize(width, height);}
+{ TwWindowSize(width, height); }
 
 int main() {
   if (!glfwInit()) return -1;
@@ -31,11 +41,12 @@ int main() {
   float vec[3] = { 1, 2, 3 };
   TwAddVarRW(twBar, "vec", TW_TYPE_DIR3F, &vec, "");
 
-  glfwSetMouseButtonCallback(window, (GLFWmousebuttonfun)TwEventMouseButtonGLFW);
-  glfwSetCursorPosCallback(window, (GLFWcursorposfun)TwMouseMotion);
-  glfwSetKeyCallback(window, (GLFWkeyfun)TwEventKeyGLFW);
-  glfwSetCharCallback(window, (GLFWcharfun)TwEventCharGLFW);
-  glfwSetWindowSizeCallback(window, TwEventWindowSizeGLFW3);
+  glfwSetMouseButtonCallback(window, (GLFWmousebuttonfun)TwEventMouseButtonGLFW3);
+  glfwSetCursorPosCallback  (window, (GLFWcursorposfun)  TwEventMousePosGLFW3);
+  glfwSetScrollCallback     (window, (GLFWscrollfun)     TwEventMouseWheelGLFW3);
+  glfwSetKeyCallback        (window, (GLFWkeyfun)        TwEventKeyGLFW3);
+  glfwSetCharCallback       (window, (GLFWcharfun)       TwEventCharGLFW3);
+  glfwSetWindowSizeCallback (window, (GLFWwindowsizefun) TwEventWindowSizeGLFW3);
 
   while (!glfwWindowShouldClose(window) && !glfwGetKey(window, GLFW_KEY_ESCAPE)) {
     glClearColor(0.4f, 0.4f, 0.4f, 1.0f);
